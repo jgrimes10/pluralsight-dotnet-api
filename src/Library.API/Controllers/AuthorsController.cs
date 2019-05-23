a426c396f3d2837d0fbc3b284a09102bdfc4e6e2
Justin Grimes <jgrimes@relias.com>
2019-05-23 15:53:55 -0400

    Add AuthorsController with GetAuthors method for getting all authors

---
+++ src/Library.API/Controllers/AuthorsController.cs
@@ -0,0 +1,23 @@
+using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.API.Controllers
+{
+    [Route("api/authors")]
+    public class AuthorsController : Controller
+    {
+        private readonly ILibraryRepository _libraryRepository;
+
+        public AuthorsController(ILibraryRepository libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _libraryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+        }
+    }
+}
